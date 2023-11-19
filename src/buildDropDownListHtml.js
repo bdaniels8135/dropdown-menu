38588fe18bf329ede6cdadfe429588c44325c1cf
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-18 23:11:08 -0500

    Make dropdown list builder into reg function

--- src/buildDropDownListHtml.js
+++ src/buildDropDownListHtml.js
@@ -1,13 +1,11 @@
 import { buildListItemHtml } from "./htmlBuilders";
 
 export default function buildDropDownListHtml(iconTextClickFuncObjects) {
-  return (() => {
-    const dropDownListHtml = document.createElement("ul");
-    iconTextClickFuncObjects.forEach(({ icon, text, clickFunc }) => {
-      const dropDownListItemHtml = buildListItemHtml(icon, text, clickFunc);
-      dropDownListItemHtml.classList.add("drop-down-list-item");
-      dropDownListHtml.appendChild(dropDownListItemHtml);
-    });
-    return dropDownListHtml;
-  })();
+  const dropDownListHtml = document.createElement("ul");
+  iconTextClickFuncObjects.forEach(({ icon, text, clickFunc }) => {
+    const dropDownListItemHtml = buildListItemHtml(icon, text, clickFunc);
+    dropDownListItemHtml.classList.add("drop-down-list-item");
+    dropDownListHtml.appendChild(dropDownListItemHtml);
+  });
+  return dropDownListHtml;
 }
